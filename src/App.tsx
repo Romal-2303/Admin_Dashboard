e1d2bdb21593e8a661e162bba74b19e168ad51ca
Romal Gaikwad <romal@Romals-MacBook-Air.local>
2024-05-25 14:19:51 +0530

    WIP -- Dashboard admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.module.scss";
 import Layout from "./Pages/Layout/Layout.tsx";
+import AdminPage from "./Pages/AdminPage.tsx";
 
 function App() {
   return (
     <div className="App">
-      <Layout></Layout>
+      <Layout>
+        <AdminPage />
+      </Layout>
     </div>
   );
 }
---
+++ src/Pages/AdminPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Admin.module.scss";
+
+const AdminPage = () => {
+  return <div className={classes["admin-dashboard-container"]}>Dashboard</div>;
+};
+
+export default AdminPage;
