2719444f4f24c0ad2f98b61401832d22157ed8fc
Garima2098 <garimasinghh2098@gmail.com>
2024-05-29 10:00:45 +0530

    button component is completed

--- src/Pages/Dashboard/AdminPage.tsx
+++ src/Pages/Dashboard/AdminPage.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Admin.module.scss";
 import styles from "../../DesignSystem/_classes.module.scss";
+import DatePicker from "react-date-picker";
+import Button from "../../component/Button/Button.tsx";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
 
 const AdminPage = () => {
-  let statsTileArr = [{ header: "" }, {}, {}, {}];
+  const clickhandler = () => {
+    alert("button clicked");
+  };
 
   return (
     <div className={classes["admin-dashboard-container"]}>
-      <div className={classes["admin-dashboard-header"]}></div>
+      <div className={classes["admin-dashboard-header"]}>
+        <div className={classes["admin-dashboard-heading"]}>Dashboard</div>
+        <div className={classes["admin-dashboard-leftcontainer"]}>
+          <div className={classes["admin-dashboard-daterange"]}></div>
+          <div className={classes["admin-dashboard-downloadbutton"]}>
+            <Button onClick={clickhandler} className={classes["custom-class"]}>
+              Downloads
+            </Button>
+          </div>
+        </div>
+      </div>
       <div
         className={`${classes["admin-dashboard-content"]} ${styles["grid-columns-4"]}`}
       >
